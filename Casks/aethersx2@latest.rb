db53e2965bd0c40fd1538785e8c00eb58b0a8816
Junian Triajianto <3881052+junian@users.noreply.github.com>
2024-09-23 21:53:02 +0700

    Update aethersx2@latest.rb

--- Casks/aethersx2@latest.rb
+++ Casks/aethersx2@latest.rb
@@ -2,7 +2,7 @@ cask "aethersx2@latest" do
   version "1.5-3939"
   sha256 "55178a1fbdca463750c5b13dbc972de290a7dce0795291d81e9c76594bc08150"
 
-  url "https://github.com/junian/homebrew-apps/releases/download/latest/AetherSX2-v#{version}-mac.zip"
+  url "https://github.com/junian/homebrew-mirrors/releases/download/latest/AetherSX2-v#{version}-mac.zip"
   name "AetherSX2"
   desc "Sony PlayStation 2 emulator for ARM based Macs"
   homepage "https://aethersx2.com/"
