672f4011f2c1e9a44ce359cd701a32b2b3be9725
Junian Triajianto <3881052+junian@users.noreply.github.com>
2024-12-17 10:16:22 +0700

    Create screen-studio@2.rb

---
+++ Casks/screen-studio@2.rb
@@ -0,0 +1,36 @@
+cask "screen-studio@2" do
+  arch arm: "-arm64"
+
+  version "2.25.33"
+  sha256 arm:   "e5c5b73a7a7e3d3ca297a23e21f66820b3d234891ff40802715110ac1d45aae7",
+         intel: "41f3990c634f764d6fe37afba25b8da0e75152d04adb4746e49c29c90a4df28e"
+
+  url "https://screenstudioassets.com/Screen%20Studio-#{version}#{arch}-mac.zip",
+      verified: "screenstudioassets.com/"
+  name "Screen Studio"
+  desc "Screen recorder and editor"
+  homepage "https://screen.studio/"
+
+  livecheck do
+    url "https://screen.studio/api/trpc/appInfo.latestVersionInfo?input=%7B%22isBeta%22%3Afalse%7D"
+    strategy :json do |json|
+      json.dig("result", "data", "version")
+    end
+  end
+
+  auto_updates true
+  depends_on macos: ">= :high_sierra"
+
+  app "Screen Studio.app"
+
+  zap trash: [
+        "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.timpler.screenstudio.sfl*",
+        "~/Library/Application Support/Screen Studio",
+        "~/Library/Caches/com.timpler.screenstudio",
+        "~/Library/Caches/com.timpler.screenstudio.ShipIt",
+        "~/Library/HTTPStorages/com.timpler.screenstudio",
+        "~/Library/Preferences/com.timpler.screenstudio.plist",
+        "~/Library/Saved Application State/com.timpler.screenstudio.savedState",
+      ],
+      rmdir: "~/Screen Studio Projects"
+end
