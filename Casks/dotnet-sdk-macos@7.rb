345562a7f50775f67dae30641e9214cdf9f8f5d4
junian <3881052+junian@users.noreply.github.com>
2025-09-23 14:37:47 +0700

    Refactor dotnet-sdk-macos@7.rb for clarity and updates

--- Casks/dotnet-sdk-macos@7.rb
+++ Casks/dotnet-sdk-macos@7.rb
@@ -1,47 +1,50 @@
 cask "dotnet-sdk-macos@7" do
   arch arm: "arm64", intel: "x64"
 
-  on_arm do
-    version "7.0.410"
-    sha256 "c7c16c5e2e5c22c73e62a5d67c9aff15db5e65ad9905ec1deadfb86d3daef0a8"
-  end
-  on_intel do
-    version "7.0.410"
-    sha256 "42eab8294702d3f206031829eebda95315c6f47944f65e6462ed14f5730d6cb5"
-  end
+  version "7.0.410"
+  sha256 arm:   "c7c16c5e2e5c22c73e62a5d67c9aff15db5e65ad9905ec1deadfb86d3daef0a8",
+         intel: "42eab8294702d3f206031829eebda95315c6f47944f65e6462ed14f5730d6cb5"
 
   url "https://builds.dotnet.microsoft.com/dotnet/Sdk/#{version}/dotnet-sdk-#{version}-osx-#{arch}.pkg"
   name ".NET SDK"
   desc "Developer platform"
   homepage "https://www.microsoft.com/net/core#macos"
 
+  # This identifies releases with the same major/minor version as the current
+  # cask version. New major/minor releases occur annually in November and the
+  # check will automatically update its behavior when the cask is updated.
   livecheck do
-    cask "dotnet"
-    regex(%r{/download/pr/([^/]+)/([^/]+)/dotnet-sdk-v?(\d+(?:\.\d+)+)-osx-#{arch}\.pkg}i)
+    url "https://builds.dotnet.microsoft.com/dotnet/release-metadata/#{version.major_minor}/releases.json"
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+    strategy :json do |json, regex|
+      json["releases"]&.map do |release|
+        version = release.dig("sdk", "version")
+        next unless version&.match(regex)
+
+        version
+      end
+    end
   end
 
-  # conflicts_with cask: [
-  #   "dotnet",
-  #   "homebrew/cask-versions/dotnet-preview",
-  #   "homebrew/cask-versions/dotnet-sdk-preview",
-  # ], formula: "dotnet"
-  depends_on macos: ">= :mojave"
+  depends_on macos: ">= :ventura"
 
   pkg "dotnet-sdk-#{version}-osx-#{arch}.pkg"
-  # binary "/usr/local/share/dotnet/dotnet"
+  binary "/usr/local/share/dotnet/dotnet"
 
   uninstall pkgutil: [
-              "com.microsoft.dotnet.dev.7.*",
-              "com.microsoft.dotnet.hostfxr.7.*",
-              "com.microsoft.dotnet.pack.apphost.7.*",
-              "com.microsoft.dotnet.pack.targeting.7.*",
-              "com.microsoft.dotnet.sharedframework.Microsoft.NETCore.App.7.*",
-            ]
-
-  zap trash: [
-    "~/.dotnet",
-    "~/.nuget",
-    "/etc/paths.d/dotnet",
-    "/etc/paths.d/dotnet-cli-tools",
+    "com.microsoft.dotnet.*#{version.major_minor}*#{arch}",
   ]
+
+  zap pkgutil: [
+        "com.microsoft.dotnet.*",
+        "com.microsoft.netstandard.pack.targeting.*",
+      ],
+      delete:  [
+        "/etc/paths.d/dotnet",
+        "/etc/paths.d/dotnet-cli-tools",
+      ],
+      trash:   [
+        "~/.dotnet",
+        "~/.nuget",
+      ]
 end
