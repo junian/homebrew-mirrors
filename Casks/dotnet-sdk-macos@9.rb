ec54fa2d4de2066f330c7b1446dcbda6b1455f0d
junian <3881052+junian@users.noreply.github.com>
2025-09-23 13:57:24 +0700

    Refactor dotnet-sdk-macos@9.rb for architecture support

--- Casks/dotnet-sdk-macos@9.rb
+++ Casks/dotnet-sdk-macos@9.rb
@@ -1,14 +1,9 @@
 cask "dotnet-sdk-macos@9" do
   arch arm: "arm64", intel: "x64"
 
-  on_arm do
-    version "9.0.305"
-    sha256 "7bbaf37675331cc58704bd4201800e8049658f4670ba256c86e7c42d49c7389b"
-  end
-  on_intel do
-    version "9.0.305"
-    sha256 "02eaf71a1485949b95d8b75e47c92439726e30eba3565e5dace0e120e9a2384d"
-  end
+  version "9.0.305"
+  sha256 arm:   "7bbaf37675331cc58704bd4201800e8049658f4670ba256c86e7c42d49c7389b",
+         intel: "02eaf71a1485949b95d8b75e47c92439726e30eba3565e5dace0e120e9a2384d"
 
   url "https://builds.dotnet.microsoft.com/dotnet/Sdk/#{version}/dotnet-sdk-#{version}-osx-#{arch}.pkg"
   name ".NET SDK"
@@ -19,39 +14,43 @@ cask "dotnet-sdk-macos@9" do
   # cask version. New major/minor releases occur annually in November and the
   # check will automatically update its behavior when the cask is updated.
   livecheck do
-    url "https://dotnetcli.blob.core.windows.net/dotnet/release-metadata/#{version.major_minor}/releases.json"
-    regex(%r{/download/pr/([^/]+)/([^/]+)/dotnet-sdk[._-]v?(\d+(?:\.\d+)+)[._-]osx[._-]#{arch}\.pkg}i)
+    url "https://builds.dotnet.microsoft.com/dotnet/release-metadata/#{version.major_minor}/releases.json"
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
     strategy :json do |json, regex|
       json["releases"]&.map do |release|
-        release.dig("sdk", "files")&.map do |file|
-          file["url"]&.scan(regex)&.map { |match| "#{match[2]},#{match[0]},#{match[1]}" }
-        end
-      end&.flatten
+        version = release.dig("sdk", "version")
+        next unless version&.match(regex)
+
+        version
+      end
     end
   end
 
-  # conflicts_with cask: [
-  #   "dotnet",
-  #   "dotnet-sdk@preview",
-  #   "dotnet@preview",
-  # ], formula: "dotnet"
-  depends_on macos: ">= :mojave"
+  conflicts_with cask: [
+    "dotnet-runtime",
+    "dotnet-runtime@preview",
+    "dotnet-sdk@8",
+    "dotnet-sdk@preview",
+  ]
+  depends_on macos: ">= :ventura"
 
   pkg "dotnet-sdk-#{version}-osx-#{arch}.pkg"
-  # binary "/usr/local/share/dotnet/dotnet"
+  binary "/usr/local/share/dotnet/dotnet"
 
   uninstall pkgutil: [
-              "com.microsoft.dotnet.dev.9.*",
-              "com.microsoft.dotnet.hostfxr.9.*",
-              "com.microsoft.dotnet.pack.apphost.9.*",
-              "com.microsoft.dotnet.pack.targeting.9.*",
-              "com.microsoft.dotnet.sharedframework.Microsoft.NETCore.App.9.*",
-            ]
-
-  zap trash: [
-    "~/.dotnet",
-    "~/.nuget",
-    "/etc/paths.d/dotnet",
-    "/etc/paths.d/dotnet-cli-tools",
+    "com.microsoft.dotnet.*#{version.major_minor}*#{arch}",
   ]
+
+  zap pkgutil: [
+        "com.microsoft.dotnet.*",
+        "com.microsoft.netstandard.pack.targeting.*",
+      ],
+      delete:  [
+        "/etc/paths.d/dotnet",
+        "/etc/paths.d/dotnet-cli-tools",
+      ],
+      trash:   [
+        "~/.dotnet",
+        "~/.nuget",
+      ]
 end
