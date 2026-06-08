988fbfb1586d937d80d7ad1ab60fb659fde7dd72
Junian Triajianto <3881052+junian@users.noreply.github.com>
2026-06-08 14:13:00 +0700

    Update windows-app-for-mac.rb

--- Casks/windows-app-for-mac.rb
+++ Casks/windows-app-for-mac.rb
@@ -1,22 +1,36 @@
 cask "windows-app-for-mac" do
-  version "11.3.5"
-  sha256 "f725f2a4e48203c6e943d11507f0467938433aeafd220e950456f63638ac05fb"
+  if MacOS.version == :ventura
+    version "11.3.3"
+    sha256 "2cd505c3b0b042477276dd8beb48bd0162aaaab5b3aaab1c6954f219a2a74ec5"
+    
+    url "https://officecdnmac.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Windows_App_#{version}_installer.pkg",
+        verified: "officecdnmac.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/"
+    
+    pkg "Windows_App_#{version}_installer.pkg"
+  else
+    # For Sonoma or later, redirect to the official core cask
+    version "11.3.5"
+    sha256 :no_check # URL/SHA isn't strictly used since depends_on fires first
 
-  url "https://officecdnmac.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Windows_App_#{version}_installer.pkg",
-      verified: "officecdnmac.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/"
+    url "https://aka.ms/WindowsApp" # Dummy/Homepage fallback URL to pass parsing
+    
+    depends_on cask: "windows-app"
+  end
+
   name "Windows App"
   desc "Connect to Windows"
   homepage "https://aka.ms/WindowsApp"
 
+  # Only run livecheck on Ventura to lock down old versions, or disable entirely
   livecheck do
+    skip "Legacy version tracking for macOS Ventura" if MacOS.version == :ventura
     url "https://go.microsoft.com/fwlink/?linkid=868963"
     strategy :header_match
   end
 
   conflicts_with cask: "microsoft-remote-desktop"
-  depends_on macos: :sonoma
-
-  pkg "Windows_App_#{version}_installer.pkg"
+  conflicts_with cask: "junian/mirrors/microsoft-remote-desktop-app"
+  depends_on macos: ">= :ventura"
 
   uninstall launchctl: [
               "com.microsoft.autoupdate.helper",
