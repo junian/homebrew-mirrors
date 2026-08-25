0bafac0ddd1599227214596c4897d3ac913f25e5
Junian Triajianto <3881052+junian@users.noreply.github.com>
2026-08-26 05:33:54 +0700

    Update windows-app-for-mac.rb

--- Casks/windows-app-for-mac.rb
+++ Casks/windows-app-for-mac.rb
@@ -26,8 +26,8 @@ cask "windows-app-for-mac" do
     end
   end
   
-  url "https://officecdnmac.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Windows_App_#{version}_installer.pkg",
-      verified: "officecdnmac.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/"
+  url "https://officecdn.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Windows_App_#{version}_installer.pkg",
+      verified: "officecdn.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/"
   name "Windows App"
   desc "Connect to Windows"
   homepage "https://aka.ms/WindowsApp"
